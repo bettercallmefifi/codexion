7d10248fb8e94acf854cb09e12c49c03f0f50a10
bettercallmefifi <ferdaouskai@gmail.com>
2026-09-16 18:41:09 +0100

    add time utils

--- src/time_utils.c
+++ src/time_utils.c
@@ -19,3 +19,22 @@ long long	now_ms(void)
 	gettimeofday(&tv, NULL);
 	return ((long long)tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
+
+void	ms_to_timespec(long long ms, struct timespec *ts)
+{
+	ts->tv_sec = ms / 1000;
+	ts->tv_nsec = (ms % 1000) * 1000000;
+}
+
+void	precise_sleep(t_sim *sim, long long ms)
+{
+	long long	end;
+
+	end = now_ms() + ms;
+	while (now_ms() < end)
+	{
+		if (!sim_running(sim))
+			return ;
+		usleep(200);
+	}
+}
