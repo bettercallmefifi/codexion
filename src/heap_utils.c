7956dedf9e7ab0ae5327fa4d4ad395d99974e17f
bettercallmefifi <ferdaouskai@gmail.com>
2026-09-16 20:19:48 +0100

    add heap utils

--- src/heap_utils.c
+++ src/heap_utils.c
@@ -10,3 +10,76 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "codexion.h"
+
+int	heap_less(t_heap *heap, t_request *a, t_request *b)
+{
+	if (heap->mode == MODE_EDF)
+	{
+		if (a->deadline != b->deadline)
+			return (a->deadline < b->deadline);
+		return (a->id < b->id);
+	}
+	return (a->seq < b->seq);
+}
+
+void	heap_swap(t_request *a, t_request *b)
+{
+	t_request	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	sift_up(t_heap *heap, int i)
+{
+	int	parent;
+
+	while (i > 0)
+	{
+		parent = (i - 1) / 2;
+		if (!heap_less(heap, &heap->data[i], &heap->data[parent]))
+			return ;
+		heap_swap(&heap->data[i], &heap->data[parent]);
+		i = parent;
+	}
+}
+
+void	sift_down(t_heap *heap, int i)
+{
+	int	child;
+
+	child = 2 * i + 1;
+	while (child < heap->size)
+	{
+		if (child + 1 < heap->size
+			&& heap_less(heap, &heap->data[child + 1], &heap->data[child]))
+			child++;
+		if (!heap_less(heap, &heap->data[child], &heap->data[i]))
+			return ;
+		heap_swap(&heap->data[child], &heap->data[i]);
+		i = child;
+		child = 2 * i + 1;
+	}
+}
+
+int	heap_remove(t_heap	*heap, int i)
+{
+	int	i;
+
+	i = 0;
+	while (i < heap->size)
+	{
+		if (heap->data[i].id == id)
+		{
+			heap->size--;
+			heap->data[i] == heap->data[heap->size];
+			sift_down(heap, i);
+			sift_up(heap, i);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
