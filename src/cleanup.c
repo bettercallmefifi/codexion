4249de944b2de7d08dc34d5db557b55e32e62e47
bettercallmefifi <ferdaouskai@gmail.com>
2026-09-16 20:26:53 +0100

    add cleanup memory and destroy the mutex and cond variable

--- src/cleanup.c
+++ src/cleanup.c
@@ -10,3 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "codexion.h"
+
+static void	destroy_dongles(t_sim *sim)
+{
+	int	i;
+
+	if (!sim->dongles)
+		return ;
+	i = 0;
+	while (i < sim->ready)
+	{
+		pthread_mutex_destroy(&sim->dongles[i].lock);
+		pthread_cond_destroy(&sim->dongles[i].cond);
+		i++;
+	}
+	i = 0;
+	while (i < sim->nb_coders)
+	{
+		heap_free(&sim->dongles[i].queue);
+		i++;
+	}
+	free(sim->dongles);
+	sim->dongles = NULL;
+}
+
+void	destroy_sim(t_sim *sim)
+{
+	destroy_dongles(sim);
+	free(sim->coders);
+	sim->coders = NULL;
+	pthread_mutex_destroy(&sim->state);
+	pthread_mutex_destroy(&sim->print);
+}
